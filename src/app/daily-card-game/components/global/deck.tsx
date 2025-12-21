efbc36c44c4eec2f1bb2e0def73356513154013a
Nickolus Cunningham <nickolus@pulley.com>
2025-12-21 08:31:05 -0800

    improve deck display to show overlapping cards

--- src/app/daily-card-game/components/global/deck.tsx
+++ src/app/daily-card-game/components/global/deck.tsx
@@ -2,9 +2,22 @@
 
 import { useState } from 'react'
 
+import { Card } from '@/app/daily-card-game/components/gameplay/card'
 import { PlayingCard } from '@/app/daily-card-game/domain/playing-card/types'
 import { useGameState } from '@/app/daily-card-game/useGameState'
 import { Button } from '@/components/ui/button'
+
+const CardRow = ({ cards }: { cards: PlayingCard[] }) => {
+  return (
+    <div className="flex flex-wrap gap-2">
+      {cards.map((card, index) => (
+        <div key={card.id} className={index !== 0 ? '-ml-16' : ''}>
+          <Card playingCard={card} />
+        </div>
+      ))}
+    </div>
+  )
+}
 
 export const Deck = () => {
   const { game } = useGameState()
@@ -13,17 +26,21 @@ export const Deck = () => {
   const [deckType, setDeckType] = useState<'remaining' | 'full'>('remaining')
 
   const deck = deckType === 'remaining' ? remainingDeck : fullDeck
+  const spades = deck.filter(card => card.suit === 'spades')
+  const hearts = deck.filter(card => card.suit === 'hearts')
+  const clubs = deck.filter(card => card.suit === 'clubs')
+  const diamonds = deck.filter(card => card.suit === 'diamonds')
 
   return (
     <div>
       <Button onClick={() => setDeckType(deckType === 'remaining' ? 'full' : 'remaining')}>
         Show {deckType === 'remaining' ? 'Full' : 'Remaining'} Deck
       </Button>
-      {deck.map((card: PlayingCard) => (
-        <p key={card.id}>
-          {card.value} {card.suit}
-        </p>
-      ))}
+
+      <CardRow cards={spades} />
+      <CardRow cards={hearts} />
+      <CardRow cards={clubs} />
+      <CardRow cards={diamonds} />
     </div>
   )
 }
