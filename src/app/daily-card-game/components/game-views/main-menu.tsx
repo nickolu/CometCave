01d74a47a18f1aa87b32fcbd24bc8368b04488d1
Nickolus Cunningham <nickolus@pulley.com>
2025-12-22 17:29:04 -0800

    add a jokers view and more jokers

--- src/app/daily-card-game/components/game-views/main-menu.tsx
+++ src/app/daily-card-game/components/game-views/main-menu.tsx
@@ -5,15 +5,43 @@ import { Button } from '@/components/ui/button'
 
 export function MainMenuView() {
   return (
-    <div>
-      <h1>Main Menu</h1>
+    <div className="flex flex-col items-center mt-10 h-screen">
+      <h1 className="text-2xl font-bold">Cosmic Cards</h1>
       <Button
+        className="mt-4"
+        size="lg"
         onClick={() => {
           eventEmitter.emit({ type: 'GAME_START' })
         }}
       >
         Start Game
       </Button>
+      <div className="flex gap-2 mt-4">
+        <Button
+          className="mt-4"
+          variant="space"
+          onClick={() => {
+            eventEmitter.emit({ type: 'DISPLAY_JOKERS' })
+          }}
+        >
+          Jokers
+        </Button>
+        <Button className="mt-4" variant="space" disabled={true}>
+          Tags
+        </Button>
+        <Button className="mt-4" variant="space" disabled={true}>
+          Vouchers
+        </Button>
+        <Button className="mt-4" variant="space" disabled={true}>
+          Arcana Cards
+        </Button>
+        <Button className="mt-4" variant="space" disabled={true}>
+          Celestial Cards
+        </Button>
+        <Button className="mt-4" variant="space" disabled={true}>
+          Spectral Cards
+        </Button>
+      </div>
     </div>
   )
 }
--- src/app/daily-card-game/page.tsx
+++ src/app/daily-card-game/page.tsx
@@ -3,6 +3,7 @@ import { BlindRewardsView } from './components/game-views/blind-rewards'
 import { BlindSelectionView } from './components/game-views/blind-selection'
 import { GameOverView } from './components/game-views/game-over'
 import { GamePlayView } from './components/game-views/gameplay'
+import JokersView from './components/game-views/jokers'
 import { MainMenuView } from './components/game-views/main-menu'
 import { PackOpenView } from './components/game-views/pack-open'
 import { ShopView } from './components/game-views/shop'
@@ -28,6 +29,8 @@ export default function DailyCardGamePage() {
       return <GameOverView />
     case 'blindRewards':
       return <BlindRewardsView />
+    case 'jokers':
+      return <JokersView />
     default:
       return <div>Error: Unknown game phase {game.gamePhase}</div>
   }
