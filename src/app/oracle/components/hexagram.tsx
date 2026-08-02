8ef95b154a06d6a1717542416d6fd8d37ce214cb
CHRONOS Executive Core <chronos-agent@bot.internal>
2026-08-02 02:04:53 +0000

    fix: resolve issue #2657

--- src/app/oracle/components/hexagram.tsx
+++ src/app/oracle/components/hexagram.tsx
@@ -1,3 +1,4 @@
+```tsx
 import { ChangingLine, ChangingLines } from '@/app/oracle/types'
 import { cn } from '@/lib/utils'
 
@@ -17,7 +18,7 @@ export const SolidLine = ({ isStatic = false }: { isStatic?: boolean }) => {
 export const EmptyLine = ({ isStatic = false }: { isStatic?: boolean }) => {
   return (
     <div
-      className={`w-44 h-4 border b-2 ${lineBorder} opacity-[0.4] hover:opacity-60 transition-all duration-200 ${isStatic ? '' : 'cursor-pointer'}`}
+      className={`w-44 h-4 border-2 ${lineBorder} opacity-[0.4] hover:opacity-60 transition-all duration-200 ${isStatic ? '' : 'cursor-pointer'}`}
     />
   )
 }
@@ -46,7 +47,7 @@ export const ChangeMarker = ({
         `w-4 h-4 rounded-full transition-all duration-200 ${isStatic ? '' : 'cursor-pointer'}`,
         hasChanges
           ? `${lineBg} opacity-[0.4]`
-          : `bg-transparent border b-4 ${lineBorder} opacity-[0.4] hover:opacity-60`
+          : `bg-transparent border-4 ${lineBorder} opacity-[0.4] hover:opacity-60`
       )}
     />
   )
@@ -107,86 +108,9 @@ export const LineControl = ({
         tabIndex={isStatic ? undefined : 0}
         onKeyDown={isStatic ? undefined : e => { if (e.key === 'Enter' || e.key === ' ') { e.preventDefault(); toggleHasChanges() } }}
       >
-        <ChangeMarker hasChanges={Boolean(lineData?.hasChanges)} isStatic={isStatic} />
+        <ChangeMarker hasChanges={lineData?.hasChanges ?? false} isStatic={isStatic} />
       </div>
     </div>
   )
 }
-
-export const Hexagram = ({
-  hexagramData,
-  setChangingLines,
-}: {
-  hexagramData: ChangingLines // these should already be in reverse order (bottom is line 1 and top is line 6)
-  setChangingLines?: (hexagramData: ChangingLines) => void
-}) => {
-  const line1 = hexagramData[0]
-  const line2 = hexagramData[1]
-  const line3 = hexagramData[2]
-  const line4 = hexagramData[3]
-  const line5 = hexagramData[4]
-  const line6 = hexagramData[5]
-
-  const toggleLine = (lineIndex: number) => {
-    const updatedChangingLines: ChangingLines = [...hexagramData]
-    if (updatedChangingLines[lineIndex] === undefined) {
-      updatedChangingLines[lineIndex] = { type: 'solid', hasChanges: false }
-    } else if (updatedChangingLines[lineIndex].type === 'solid') {
-      updatedChangingLines[lineIndex].type = 'broken'
-    } else {
-      updatedChangingLines[lineIndex].type = 'solid'
-    }
-    setChangingLines?.(updatedChangingLines)
-  }
-
-  const toggleHasChanges = (lineIndex: number) => {
-    const updatedChangingLines: ChangingLines = [...hexagramData]
-    if (updatedChangingLines[lineIndex] === undefined) {
-      return
-    }
-
-    updatedChangingLines[lineIndex].hasChanges = !updatedChangingLines[lineIndex].hasChanges
-    setChangingLines?.(updatedChangingLines)
-  }
-
-  return (
-    <div className="flex flex-col gap-4 items-center">
-      <LineControl
-        lineData={line1}
-        isStatic={Boolean(setChangingLines === undefined)}
-        toggleLine={() => toggleLine(0)}
-        toggleHasChanges={() => toggleHasChanges(0)}
-      />
-      <LineControl
-        lineData={line2}
-        isStatic={Boolean(setChangingLines === undefined)}
-        toggleLine={() => toggleLine(1)}
-        toggleHasChanges={() => toggleHasChanges(1)}
-      />
-      <LineControl
-        lineData={line3}
-        isStatic={Boolean(setChangingLines === undefined)}
-        toggleLine={() => toggleLine(2)}
-        toggleHasChanges={() => toggleHasChanges(2)}
-      />
-      <LineControl
-        lineData={line4}
-        isStatic={Boolean(setChangingLines === undefined)}
-        toggleLine={() => toggleLine(3)}
-        toggleHasChanges={() => toggleHasChanges(3)}
-      />
-      <LineControl
-        lineData={line5}
-        isStatic={Boolean(setChangingLines === undefined)}
-        toggleLine={() => toggleLine(4)}
-        toggleHasChanges={() => toggleHasChanges(4)}
-      />
-      <LineControl
-        lineData={line6}
-        isStatic={Boolean(setChangingLines === undefined)}
-        toggleLine={() => toggleLine(5)}
-        toggleHasChanges={() => toggleHasChanges(5)}
-      />
-    </div>
-  )
-}
+```
